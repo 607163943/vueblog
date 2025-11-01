d834d2a288860e59f90567f4a64779e69448edeb
607163943 <q45962219@126.com>
2025-11-01 18:21:45 +0800

    后端项目结构调整为分组聚合类型项目

---
+++ backend/pojo/src/main/java/top/hcode/blog/pojo/po/MBlog.java
@@ -0,0 +1,53 @@
+package top.hcode.blog.pojo.po;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Himit_ZH
+ * @since 2020-07-19
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class MBlog implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Long userId;
+
+    //private String author;
+
+    private String title;
+
+    private String description;
+
+    private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime gmtCreate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime gmtModified;
+
+    private Integer status;
+
+    @TableLogic
+    private Integer deleted;
+
+
+}
---
+++ backend/pojo/src/main/java/top/hcode/blog/pojo/po/MUser.java
@@ -0,0 +1,49 @@
+package top.hcode.blog.pojo.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Himit_ZH
+ * @since 2020-07-19
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class MUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "用户名不能为空")
+    private String username;
+
+    private String avatar;
+
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
+
+    private String password;
+
+    private Integer status;
+
+    private Date created;
+
+    private Date lastLogin;
+
+}
